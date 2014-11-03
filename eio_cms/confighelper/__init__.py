a9ca95f47f0bed6532df2238c60c7e4112e10d6f
Konstantin Tretjakov <kt@ut.ee>
2014-11-03 20:50:57 +0200

    Fixes

--- eio_cms/confighelper/__init__.py
+++ eio_cms/confighelper/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-import argparse, ConfigParser, os, pkg_resources
+import argparse, os, pkg_resources
 
 def replace_file(src, dest, sub):
     with open(src) as f:
@@ -13,21 +13,17 @@ def replace_file(src, dest, sub):
 def main():
     parser = argparse.ArgumentParser(description='''EIO CMS setup configuration helper script.
 
-Before invoking it make sure the current directory has the file named setup.cfg 
-present in it with the appropriately configured [eio-cms] section.
+Before invoking it make sure the current directory has the file named settings.py
+present in it with the appropriately configured values.
 
-Example of a setup.cfg file is available as a sample-setup.cfg in the package directory.''')
+Example of a settings.py file is available as a sample-setttings.py in the package directory.''')
     parser.add_argument('cms_config_file', metavar='<cms.conf>', help='full path to the cms.conf file to be written')
     args = parser.parse_args()
-    if not os.path.exists("setup.cfg"):
-        raise Exception("Please, invoke this script from the directory, that contains the file setup.cfg")
+    if not os.path.exists("settings.py"):
+        raise Exception("Please, invoke this script from the directory, that contains the file settings.py")
 
-    Config = ConfigParser.ConfigParser()
-    Config.read("setup.cfg")
-    if 'eio-cms' not in Config.sections():
-        raise Exception("Please, make sure the section [eio-cms] exists in your setup.cfg")
-    opts = {o: Config.get('eio-cms', o) for o in Config.options('eio-cms')}
-
+    opts = {}
+    execfile('settings.py', {}, opts)
     replace_file(
         pkg_resources.resource_filename('eio_cms.confighelper', 'conf/cms.conf'),
         args.cms_config_file,
---
+++ sample_settings.py
@@ -0,0 +1,4 @@
+SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://cmsuser:<password>@localhost/cmsuser"
+SECRET_KEY = "<key>"
+SECRET_USERNAME = "eio"
+SECRET_PASSWORD = "<password>"
